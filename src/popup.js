cb178e3e97d198e216db5de887bb0ca6c0c221c1
Mathieu Breton <mbreton@xebia.fr>
2014-07-31 01:34:36 +0200

    Fix game adding

--- src/popup.js
+++ src/popup.js
@@ -14,12 +14,12 @@ loadCss(url + 'tinybox.css', function () {
         html: '<%= html %>',
         close: true
     });
-});
 
-document.getElementById("game").addEventListener('click', function () {
-    var KICKASSVERSION='2.0';
-    var s = document.createElement('script');
-    s.type='text/javascript';
-    document.body.appendChild(s);
-    s.src='//hi.kickassapp.com/kickass.js';
+    document.getElementById("game").addEventListener('click', function () {
+        var KICKASSVERSION='2.0';
+        var s = document.createElement('script');
+        s.type='text/javascript';
+        document.body.appendChild(s);
+        s.src='//hi.kickassapp.com/kickass.js';
+    });
 });
