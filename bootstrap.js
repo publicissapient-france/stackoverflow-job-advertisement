3e68553f328b58e114e0728be2ae305549a1d826
Mathieu Breton <mbreton@xebia.fr>
2014-07-31 00:51:24 +0200

    Built stackoverflow-job from commit 8beb246 on branch master

--- bootstrap.js
+++
@@ -1,7 +0,0 @@
-(window.loadScript = function (url, callback) {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = url;
-    script.onload = script.onreadystatechange = callback;
-    document.getElementsByTagName('head')[0].appendChild(script);
-})('https://xebia-france.github.io/stackoverflow-job-advertisement/stack.js');
