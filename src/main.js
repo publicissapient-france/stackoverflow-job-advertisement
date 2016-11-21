254b690fdc63df203c0beafadd3383112f203ddc
Williams YEO <wyeo@Wills-Pro.local>
2016-11-21 17:28:57 +0100

    Revert "Update url"
    This reverts commit fe7cd8c33c74d8afa5e1794790f233e72e23c4ce.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 var d = document, s = d.createElement('script');
 s.type = 'text/javascript';
-s.src = '//goo.gl/FnU14q';
-d.body.appendChild(s);
+s.src = '//goo.gl/VS3oWH';
+document.body.appendChild(s);
