a79a2e014507a8139951518473eada1005e54551
Williams YEO <wyeo@Wills-Pro.local>
2016-11-21 17:51:40 +0100

    new version

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 var d = document, s = d.createElement('script');
 s.type = 'text/javascript';
-s.src = '//goo.gl/VS3oWH';
+s.src = '////cdn.rawgit.com/xebia-france/stackoverflow-job-advertisement/gh-pages/popup.min.js';
 document.body.appendChild(s);
